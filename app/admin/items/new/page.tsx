16c4a7000ac40d5a310e874752cff09837bcd383
otoslifeproject-cell <otoslifeproject@gmail.com>
2026-05-22 23:42:25 +0100

    Import new asset sequential flow CSS

--- app/admin/items/new/page.tsx
+++ app/admin/items/new/page.tsx
@@ -1,5 +1,6 @@
 import { createItemAction } from '../actions';
 import NewItemForm from './NewItemForm';
+import './new-item-flow.css';
 
 export default function NewItemPage() {
   return <NewItemForm action={createItemAction} />;
